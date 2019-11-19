6cea6c73577969d57ea5fd792b3330fecef65d1d
Kon Kouta <sound-k@rfg.rfgad.ricoh.com>
2019-11-19 11:38:28 +0900

    ライブラリを使わないmvvmでの実装

---
+++ WpfEdu/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfEdu;
+
+namespace WpfEdu.ViewModels
+{
+	class MainWindowViewModel : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public MainWindowViewModel()
+		{
+			// 初期値を設定
+			MyText = "Hello World";
+			PushCommand = new PushCommand(this);
+		}
+
+		/// <summary>
+		/// テキストボックスに表示する文字列
+		/// </summary>
+		private string myText;
+		public string MyText
+		{
+			get { return this.myText; }
+			set
+			{
+				this.myText = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		/// <summary>
+		/// ボタン押下のコマンド
+		/// </summary>
+		public ICommand PushCommand { get; private set; }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// 値が変更されたことをViewへ通知する
+		/// </summary>
+		/// <param name="propertyName"></param>
+		private void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
