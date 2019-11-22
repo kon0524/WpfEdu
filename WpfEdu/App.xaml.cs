1c99587e5f0e7ccafdea16a6007ba581c4f2148a
Kota Kon <sound.recording.man@gmail.com>
2019-11-22 13:35:30 +0900

    DIコンテナを入れてみた

--- WpfEdu/App.xaml.cs
+++ WpfEdu/App.xaml.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
+using WpfEdu.Views;
 
 namespace WpfEdu
 {
 	/// <summary>
 	/// App.xaml の相互作用ロジック
 	/// </summary>
-	public partial class App : Application
+	public partial class App : PrismApplication
 	{
+		protected override Window CreateShell()
+		{
+			return this.Container.Resolve<MainWindow>();
+		}
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+		}
 	}
 }
